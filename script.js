7c0cdcb3c01f59a9d7616180205751d3f4936708
Edith Emmings <emmings.edith@gmail.com>
2019-08-30 16:02:50 -0500

    can add to the list on the dom

---
+++ script.js
@@ -0,0 +1,91 @@
+$(readyNow); 
+
+function readyNow() {
+    $('#submitBtn').on('click', activateSubmit);
+    //submit button activation
+    //delete button activation
+}
+let employees = [];
+
+
+function activateSubmit(){
+    let firstName = $('#first-name-input').val();
+    let lastName = $('#last-name-input').val();
+    let id = $('#id-input').val();
+    let title = $('#title-input').val();
+    let salary = $('#salary-input').val();
+
+    let employee = {
+        firstName: firstName,
+        lastName: lastName,
+        id: id,
+        title: title,
+        salary: salary
+    }
+
+    employees.push(employee);
+    listOutEmployees();
+
+    $('#first-name-input').val('');
+    $('#last-name-input').val('');
+    $('#id-input').val('');
+    $('#title-input').val('');
+    $('#salary-input').val('');
+}
+
+function listOutEmployees(){
+    $('#employee-list').empty();
+    employees.forEach(function (employee){
+        console.log(employees);
+        console.log(employee);
+        console.log(employees[0]);
+        console.log(employee.firstName)
+        let $newRow = `<tr><td>`+employee.firstName+`</td><td>`+employee.lastName+`</td><td>`+employee.id+`</td><td>`+employee.title+`</td><td>`+employee.salary+`</td><td><button class="deleteBtn">Delete</button></td></tr>`;
+        $('#employee-list').append($newRow);
+    })
+}
+
+// function listOutFriends() {
+//     $('#listFriends').empty();
+//     favFoods.forEach(function (person) {
+//         let $li = $(
+//             `<li data-cohort="Porta">
+//                 ${person.name}
+//                 <button class='getFood'>${person.favoriteMeal}</button>
+//             </li>`
+//         )// end $li
+
+//         $li.data('favoriteFood', person.favoriteFood);
+
+//         $('#listFriends').append($li)//end append
+//     })//end forEach
+
+//     $('.getFood').on('click', handleClick)
+// }//end ListOutFriendss
+
+
+
+// if (name != '') {
+//     $('#name').removeClass('red-border');
+// }
+// if (meal != '') {
+//     $('#meal').removeClass('red-border');
+// }
+// if (food != '') {
+//     $('#food').removeClass('red-border');
+// }
+// if (name == '') {
+//     $('#name').addClass('red-border');
+//     alert('Please fill out the name input');
+// } else if (meal == '') {
+//     $('#meal').addClass('red-border');
+//     alert('Please fill out the meal input');
+// } else if (food == '') {
+//     $('#food').addClass('red-border');
+//     alert('Please fill out the food input');
+// } else {
+
+
+ 
+
+// }
