612e2d5893382bc2c75a008cf238392b8ebd5345
Edith Emmings <emmings.edith@gmail.com>
2019-08-30 16:42:40 -0500

    monthly totals and max out at 20000

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function readyNow() {
 }
 let employees = [];
 
-
 function activateSubmit(){
     let firstName = $('#first-name-input').val();
     let lastName = $('#last-name-input').val();
@@ -35,14 +34,20 @@ function activateSubmit(){
 
 function listOutEmployees(){
     $('#employee-list').empty();
+    let totalMonthly = 0;
     employees.forEach(function (employee){
-        console.log(employees);
-        console.log(employee);
-        console.log(employees[0]);
-        console.log(employee.firstName)
-        let $newRow = `<tr><td>`+employee.firstName+`</td><td>`+employee.lastName+`</td><td>`+employee.id+`</td><td>`+employee.title+`</td><td>`+employee.salary+`</td><td><button class="deleteBtn">Delete</button></td></tr>`;
+        let $newRow = `<tr><td>`+employee.firstName+`</td><td>`+employee.lastName+`</td><td>`+employee.id+`</td><td>`+employee.title+`</td><td>$`+employee.salary+`</td><td><button class="deleteBtn">Delete</button></td></tr>`;
         $('#employee-list').append($newRow);
+        let monthly = employee.salary / 12;
+        totalMonthly += monthly;
     })
+    $('#total-monthly').empty();
+    $('#total-monthly').append('Total Monthly: $'+totalMonthly);
+    if (totalMonthly > 20000){
+        $('#total-monthly').addClass('red');
+    } else {
+        $('#total-monthly').removeClass('red');
+    }
 }
 
 // function listOutFriends() {
